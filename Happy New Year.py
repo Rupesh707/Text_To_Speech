a42929e2eb026a5d2b9f67901429facda12f1802
Rupesh <73946655+Rupesh707@users.noreply.github.com>
2020-12-30 12:53:34 +0100

    Seasonal Greetings

---
+++ Happy New Year.py
@@ -0,0 +1,19 @@
+# Importing packages
+from gtts import gTTS
+import os
+
+#Reading the text
+Readmytext = open("Greetings.txt", "r")
+Text = fh.read().replace("\n", " ")
+
+Language ='en'
+
+output = gTTS(text=Readmytext, lang = Language, slow=False)
+
+#Saving output
+output.save("Happy New Year 2021.mp3")
+
+Readmytext.close()
+
+#Playing output
+os.system("start Happy New Year 2021.mp3")
